877fc44e1d83cf73c43f7fa1ae39b867bff523a2
Louis Poudrier-Racette <lpracette@gmail.com>
2025-04-07 10:44:41 -0400

    brew pkgs

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ DOTFILES="$( cd "$( dirname "${BASH_SOURCE[0]}")" && pwd )"
 # Install Homebrew
 if ! command -v brew > /dev/null; then
   /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  eval "$(/opt/homebrew/bin/brew shellenv)" # https://docs.brew.sh/FAQ#why-should-i-install-homebrew-in-the-default-location
 fi
 
 # Install Homebrew packages
