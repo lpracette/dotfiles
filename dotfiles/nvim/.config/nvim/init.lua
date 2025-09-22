63f552272b17739aa76908a35f7258f238d9f4f7
Louis Poudrier-Racette <lpracette@gmail.com>
2025-09-22 08:15:17 -0400

    feat(nvim): add keymaps for inserting date-time, markdown h3, opening in vscode, and converting JSON/YAML

--- dotfiles/nvim/.config/nvim/init.lua
+++ dotfiles/nvim/.config/nvim/init.lua
@@ -45,6 +45,42 @@ vim.opt.laststatus = 2
 vim.api.nvim_set_keymap('i', 'jk', '<esc>', { noremap = true })
 vim.api.nvim_set_keymap('c', 'bb', 'b#', { noremap = true })
 
+-- Insert current date-time in normal mode
+vim.keymap.set('n', '<leader>idn', function() vim.api.nvim_put({ os.date('%c') }, 'c', true, true) end, { silent = true, desc = '[i]nsert current [d]ate-time' })
+
+-- Insert current date-time using commentstring
+vim.keymap.set('n', '<leader>idc', function()
+  local comment = vim.bo.commentstring
+  if not comment or not comment:find('%%s') then comment = '# %s' end
+  local text = string.format(comment, ' ' .. os.date('%c') .. ' ')
+  vim.api.nvim_put({ text }, 'c', true, true)
+end, { silent = true, desc = '[i]nsert current [d]ate-time as [c]omment' })
+
+-- Add a markdown h3 with date
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'markdown',
+  callback = function()
+    vim.keymap.set('n', '<leader>idm', function()
+      local title = vim.fn.input('Note title: ')
+      local line = string.format('### %s (%s)', title, os.date('%c'))
+      local row = vim.api.nvim_win_get_cursor(0)[1]
+      vim.api.nvim_buf_set_lines(0, row, row, false, { line })
+    end, { buffer = true, silent = true, desc = '[i]nsert [d]ate-time as [m]arkdown h3' })
+  end,
+})
+
+-- Open current file in vscode
+vim.keymap.set('n', '<leader>v', function()
+  local cmd = string.format('code %s --goto %s:%d:%d', vim.fn.getcwd(), vim.fn.expand('%'), vim.fn.line('.'), vim.fn.col('.'))
+  vim.fn.system(cmd)
+end, { silent = true })
+
+-- Convert JSON to YAML with python3
+vim.keymap.set('x', '<leader>ty', ":'<,'>!python3 -c 'import sys, json, yaml; yaml.safe_dump(json.load(sys.stdin), sys.stdout, default_flow_style=False)'<CR>", { silent = true, noremap = true, expr = false })
+
+-- Convert YAML to JSON with python3
+vim.keymap.set('x', '<leader>tj', ":'<,'>!python3 -c 'import sys, json, yaml;print(json.dumps(yaml.load(sys.stdin,Loader=yaml.FullLoader), indent=2,default=str))'<CR>", { silent = true, noremap = true, expr = false })
+
 -- Highlighting
 vim.api.nvim_set_hl(0, 'CopilotSuggestion', { fg = '#808080' })
 
