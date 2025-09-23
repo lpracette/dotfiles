d4e5d4cf156643b58c8b0b3f058882d39fe24853
Louis Poudrier-Racette <lpracette@gmail.com>
2025-09-23 15:32:44 -0400

    feat(nvim): show nvim-dap status in lualine when debugging session is active

--- dotfiles/nvim/.config/nvim/lua/plugins/ui.lua
+++ dotfiles/nvim/.config/nvim/lua/plugins/ui.lua
@@ -20,6 +20,17 @@ return {
     opts = {
       sections = {
         lualine_x = { 'copilot', 'encoding', 'fileformat', 'filetype' },
+        lualine_c = {
+          {
+            function() return require('dap').status() end,
+            icon = { '', color = { fg = '#e7c664' } }, -- nerd icon.
+            cond = function()
+              if not package.loaded.dap then return false end
+              local session = require('dap').session()
+              return session ~= nil
+            end,
+          },
+        },
       },
     },
   },
